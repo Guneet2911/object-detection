f8b2d42af58e2918af6c62fdd5e995d148661989
Guneet2911 <98748570+Guneet2911@users.noreply.github.com>
2022-11-30 18:33:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function next()
+{
+    window.location = "utensils.html";
+}
+
+function next1()
+{
+    window.location = "gadgets.html";
+}
+
+function next2()
+{
+    window.location = "bottles.html";
+}
